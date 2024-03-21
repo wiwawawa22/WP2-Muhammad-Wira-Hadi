808a8027634f156c270a11717f13dc55af7b6f90
wiwawawa22 <muhammadwirahadi2@gmail.com>
2024-03-21 13:19:02 +0700

    Tugas Contoh1

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController {
+
+    public function index() {
+
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Wira Hadi.
+              Saya tinggal di daerah Kebun Jeruk XV
+              olahraga yang saya sukai adalah Sepak Bola";
+    
+    }
+
+}
